d2a9927d88f64a7d1d93f6127c4b889fed2a1c1d
jayaprakash joshi <joshi123rocks@gmail.com>
2017-02-09 05:35:34 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,13 +9,10 @@ element.innerHTML ='cool tool you gotta be a fool';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight () {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 5;
     img.style.marginLeft = MarginLeft + 'px' ;
-    
 }
-
 img.onclick = function () {
-    img.style.marginLeft = '100px';
     var interval = setInterval(moveRight, 100);
     
 };

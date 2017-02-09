4bb8db3d1f98f17cfe696590d778af6ad0b56bfe
jayaprakash joshi <joshi123rocks@gmail.com>
2017-02-09 05:02:02 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 //change the text of the main-text div
 var element = document.getElementById('main-text');
 
-element.innerHTML ='new value';
+element.innerHTML ='cool tool you gotta be a fool';
 
 // move the image
 var img = document.getElementById('madi');

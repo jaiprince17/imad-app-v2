0ef5b7e5d4776b22d54862e807f7214fad42f0e2
jayaprakash joshi <joshi123rocks@gmail.com>
2017-02-11 08:02:08 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,9 +102,8 @@ app.get('/counter', function (req, res) {
    res.send(counter.toString());
 });
 
-
 var names = [] ;
-app.get('/submit-name', function(req, res) { // url:/submut-name?=xxxxx
+app.get('/submit-name', function(req, res) { // submit-name?=xxxxx
     // get the name from the request
     var name = req.query.name;
     

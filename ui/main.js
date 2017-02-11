f3d23ef036ce7c9b0feba8163861986e9090624d
jayaprakash joshi <joshi123rocks@gmail.com>
2017-02-11 08:46:41 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,6 @@ request.send(null);
 };
 
 // Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
@@ -54,6 +52,8 @@ submit.onclick = function () {
     // not done yet
 };
 // make the request
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
 request.open('GET','http://jaiprince17.imad.hasura-app.io/submit-name?name=' + name,true);
 request.send(null);
     

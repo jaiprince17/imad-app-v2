238d5a454be7b94ccb812ae105cb4764b4be53f6
jayaprakash joshi <joshi123rocks@gmail.com>
2017-02-11 07:36:26 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ request.send(null);
 
 };
 
-// submit name
+// Submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
@@ -35,7 +35,7 @@ submit.onclick = function () {
     // capture a list of names and render it as a list
     
     var names = ['name1','name2','name3','name4'];
-    var list = '';
+    var list = " ";
     for (var i=0; i<names.length; i++) {
         list += '<li>' + names[i] + '</li>';
     }

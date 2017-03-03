62b75c0871bbaa8817947b146a7f59b4fca964c8
jayaprakash joshi <joshi123rocks@gmail.com>
2017-03-03 08:02:40 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var crypto = require('crypto');
-var bodyParser = require('body-parser');
 
 var config = {
   host: 'db.imad.hasura-app.io',
@@ -15,7 +13,6 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser.JSON());
 
 function createTemplate (data)  {
     var title = data.title;
@@ -55,65 +52,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
-function hash(input, salt) {
+function hash (input, salt) {
     // yep how we gonna create a hash
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
-    
 }
 
-app.get('/hash/:input', function(req, res) {
-    var hashedString = hash(req.params.input, 'this-is-some-random-string');
-    res.send(hashedString);
 
-});
-
-app.get('/create-user',function (req, res) {
-    // username, password
-    // {"username": "Jaiprince17", "password": "password"}
-    // JSON
-    var username = req.body.username;
-    var password = req.body.password;
-    var salt = crypto.randomBytes(128).toString('hex');
-    var dbstring = hash(password,salt);
-    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function(err, result) {
-         if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            res.send('User Sucessfully Created:' + username);
-        }
-    });
-});
-
-app.post('/login', function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    
-    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result) {
-         if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            if (result,rows,length === 0) {
-                res.send(403),send('username/password is invalid');
-            } else {
-                //match the password
-                var dbString = result.rows[0],password;
-                var salt = dbString.split('$')[2];
-                var hashedpassword = hash(password, salt); // creating a hash based on the password submitted and the orginal salt
-                if (hashedpassword === dbstring) {
-                    res.send('credentials correct!');
-                } else {
-                    res.send(403).send('username/password is invalid');
-                }
-                
-            }
-}
-});
-});
-
-
-var pool = new Pool(config);
+var pool = new Pool(config)
 app.get('/test-db', function (req, res) {
     // make a select request
     // return a response with the results
@@ -136,7 +82,15 @@ app.get('/counter', function (req, res) {
     
 });
 
-
+var names = [] ;
+app.get('/submit-name', function(req, res) { //  /submit-name?name=xxxxx
+    // get the name from the request
+    var name = req.query.name;
+    
+    names.push(name);
+   // JSON: Javascript Object Notataion 
+    res.send(JSON.stringify(names)); 
+});
 
 app.get('/articles/:articleName', function (req, res) {
 // articleName == article-one

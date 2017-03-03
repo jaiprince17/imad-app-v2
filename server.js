2735dc0cdf80d462c2af1b7a3ce91178e4dd03c1
jayaprakash joshi <joshi123rocks@gmail.com>
2017-03-03 07:36:20 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var bodyParser = require('body-parser');
+
 
 var config = {
   host: 'db.imad.hasura-app.io',
@@ -13,6 +15,7 @@ var config = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.JSON());
 
 function createTemplate (data)  {
     var title = data.title;
@@ -52,7 +55,24 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config)
+
+function hash(input, salt) {
+    // yep how we gonna create a hash
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
+    
+}
+
+app.get('/hash/:input', function(req, res) {
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
+
+    
+});
+
+
+
+var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
     // make a select request
     // return a response with the results

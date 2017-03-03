1b409b4d8959b033ee57f8e3ac8c17e2793abf75
jayaprakash joshi <joshi123rocks@gmail.com>
2017-03-03 06:45:57 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/create-user',function (req, res) {
 });
 
 app.post('/login', function (req, res) {
-    var username = req.body.usernmae;
+    var username = req.body.username;
     var password = req.body.password;
     
     pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result) {

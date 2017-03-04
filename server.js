4c2db94d3ed6f7bf805dc3c43bbef6e973e5cc65
jayaprakash joshi <joshi123rocks@gmail.com>
2017-03-04 06:02:37 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,12 +66,18 @@ app.get('/hash/:input', function(req, res) {
     res.send(hashedString);
 });
 
-app.get('/create-user', function(req, res) {
+app.post('/create-user', function(req, res) {
    //username , password
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
    pool.query('INSERT INTO "user" (username, password) Values ($1, $2)', [username, dbString], function (err, result){
-   
+    if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send('User Successfully created: ' + username);
+            
+        }
+        
        
        });
 });

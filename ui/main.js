3a09df970d1a805695b1f9cecd26ad237d0521d4
jayaprakash joshi <joshi123rocks@gmail.com>
2017-03-06 03:06:09 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,7 +25,7 @@ var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
 console.log(username);
 console.log(password);
-request.open('POST','http://jaiprince17.imad.hasura-app.io/login', true);
+request.open('POST','/login', true);
 request.setRequestHeader('Content-Type', 'application/json');
 request.send(JSON.stringify({username:username, password:password}));
     

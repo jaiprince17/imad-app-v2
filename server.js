8e1e649c866fd9ffadca73c829bc7faa22bc394f
jayaprakash joshi <joshi123rocks@gmail.com>
2017-03-06 07:18:31 -0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ app.get('/check-login', function (req, res) {
 
 app.get('/logout', function (req, res) {
     delete req.session.auth;
-    res.send();
+    res.send('logged out');
 });
 
 var pool = new Pool(config);

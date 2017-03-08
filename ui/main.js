c4649609a5358b90fdbb475b6cce6838df6aef44
jayaprakash joshi <joshi123rocks@gmail.com>
2017-03-08 02:07:31 -0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,37 +1,33 @@
-// Submit username/password
-var submit = document.getElementById('submit_btn');
-submit.onclick = function () {
-    
-        // create a request object
+//submit username/password to login
+var submit=document.getElementById('submit_btn');
+
+submit.onclick=function(){
     var request = new XMLHttpRequest();
     
-    // capture the response and store it in a variable
-    request.onreadyStatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-        // take some action
-        if (request.status === 200) {
-          alert('logged in successfully');
-        }else if (request.status === 403) {
-            alert('username/password is incorrect');
-        }else if (request.status === 500) {
-            alert ('Something Went wrong on the server');
+    request.onreadystatechange = function(){
+        if(request.readyState ===XMLHttpRequest.DONE){
+            if(request.status===200){
+                console.log('user logged in');
+                alert('Logged in succesfully');
+            } else if(request.status===403){
+                    alert('Username/ Password is incorrect');   
+            } else if(request.status===500){
+                alert('Something went wrong on the server');
+            }
+            
         }
-        }
+    };
     
-    // not done yet
+    //Make the request
+    
+    var username=document.getElementById('username').value;
+    var password=document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','/login',true);
+    request.setRequestHeader('Content-Type','application/json');
+    request.send(JSON.stringify({username: username,password: password}));
+    
 };
-// make the request
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
-console.log(username);
-console.log(password);
-request.open('POST','http://jaiprince17.imad.hasura-app.io/login', true);
-request.setRequestHeader('Content-Type', 'application/json');
-request.send(JSON.stringify({username: username,password: password}));
-    
-};
-    
-    
-
 
 

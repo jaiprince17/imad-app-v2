3e4a221de6dab0484d2a8bdb70ae739fe8af0f7e
jayaprakash joshi <joshi123rocks@gmail.com>
2017-03-17 05:05:24 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,17 +20,17 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
     secret: 'someRandomSecretValue',
-    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}
-    
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30}
 }));
 
-function createTemplate (data)  {
+function createTemplate (data) {
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
-    var htmlTemplate =  ` 
-   <html>
+    
+    var htmlTemplate = `
+    <html>
       <head>
           <title>
               ${title}
@@ -73,84 +73,90 @@ app.get('/', function (req, res) {
 });
 
 
-function hash(input, salt) {
-    // yep how we gonna create a hash
+function hash (input, salt) {
+    // How do we create a hash?
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
     return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
 
 
 app.get('/hash/:input', function(req, res) {
-    var hashedString = hash(req.params.input, 'this is some random string');
-    res.send(hashedString);
+   var hashedString = hash(req.params.input, 'this-is-some-random-string');
+   res.send(hashedString);
 });
 
-app.post('/create-user', function(req, res) {
-   //username , password
-   // {"username": "joshi", "password": "password"}
+app.post('/create-user', function (req, res) {
+   // username, password
+   // {"username": "tanmai", "password": "password"}
    // JSON
    var username = req.body.username;
    var password = req.body.password;
    var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt);
-   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result){
-    if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            res.send('User Successfully created: ' + username);
-        }
-         });
+   pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send('User successfully created: ' + username);
+      }
+   });
 });
 
-app.post('/login', function(req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    
-    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result){
-    if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            if (result.rows.length === 0){
+app.post('/login', function (req, res) {
+   var username = req.body.username;
+   var password = req.body.password;
+   
+   pool.query('SELECT * FROM "user" WHERE username = $1', [username], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          if (result.rows.length === 0) {
+              res.status(403).send('username/password is invalid');
+          } else {
+              // Match the password
+              var dbString = result.rows[0].password;
+              var salt = dbString.split('$')[2];
+              var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
+              if (hashedPassword === dbString) {
+                
+                // Set the session
+                req.session.auth = {userId: result.rows[0].id};
+                // set cookie with a session id
+                // internally, on the server side, it maps the session id to an object
+                // { auth: {userId }}
+                
+                res.send('credentials correct!');
+                
+              } else {
                 res.status(403).send('username/password is invalid');
-                } else {
-                    // Match the password
-                    var dbString = result.rows[0].password;
-                    var salt = dbString.split('$')[2];
-                    var hashedPassword = hash(password, salt); // creating a hash based on the password submitted and the original salt 
-                    if (hashedPassword === dbString) {
-                // set the session
-                        req.session.auth = {userId: result.rows[0].id};
-                        // abcd efghijklm set cookie with a server side, 
-                        // internally, on the server side , it maps the session id to an object
-                        // {auth: {userId}}
-                         res.send('credentials correct !');
-                    } else {
-                        res.status(403).send('username/password is invalid');
-                    }
-                    
-                    
-                } 
-        }
-});
+              }
+          }
+      }
+   });
 });
 
-app.get('/check-login',function(req, res) {
-    if (req.session && req.session.auth && req.session.auth.userId) {
-        res.send('you are logged in: ' + req.session.auth.userId.toString());
-    } else {
-        res.send('you are not logged in');
-    }
-    
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
 });
 
-app.get('/logout',function(req,res){
+app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('Logged Out!!');
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
-
-
 
 var pool = new Pool(config);
+
 app.get('/get-articles', function (req, res) {
    // make a select request
    // return a response with the results
@@ -195,7 +201,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
                             if (err) {
                                 res.status(500).send(err.toString());
                             } else {
-                                res.status(200).send('Comment inserted!');
+                                res.status(200).send('Comment inserted!')
                             }
                         });
                 }
@@ -230,4 +236,5 @@ app.get('/ui/:fileName', function (req, res) {
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
+});
 });
